ceb46c5198f836dd44377d68c5c039ad41279dee
bayanRadwan <bayanwaleedoo@gmail.com>
2017-01-07 10:02:23 +0200

    Stack

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,3 +1,15 @@
 var Stack = function() {
-	//your code is here
+	this._size=0;
+	this._storage=[];
 };
+Stack.prototype.add=function(value){
+	
+	var size=this._size++
+	return this._storage.push(value);
+}
+Stack.prototype.remove=function(value){
+	var size=this._size--
+	if(._size>0){
+		return this._storage.pop();
+	}
+}
